8f036d7f9fd6de4fe7d602367b9dfa46d7928594
sumanhope <sumansthahope@gmail.com>
2024-06-28 17:36:05 +0545

    removed unused widgets

--- lib/utils/constants/enums.dart
+++
@@ -1,9 +0,0 @@
-/* --
-      LIST OF Enums
-      They cannot be created inside a class.
--- */
-
-/// Switch of Custom Brand-Text-Size Widget
-enum TextSizes { small, medium, large }
-
-enum OrderStatus { processing, shipped, delivered, pending }
